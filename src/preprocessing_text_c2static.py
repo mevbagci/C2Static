ecf1eaf5ccd732c8b617523d817daefd8b547c9b
Mevluet Bagci <bagci@em.uni-frankfurt.de>
2021-11-18 19:14:27 +0100

    Automatisation

--- src/preprocessing_text_c2static.py
+++ src/preprocessing_text_c2static.py
@@ -1,6 +1,7 @@
 import spacy
 import tqdm
 from os import makedirs
+from transformers import AutoTokenizer
 
 
 def txt_to_sentence(input_dir: str, input_name: str, lang: str):
@@ -27,5 +28,40 @@ def txt_to_sentence(input_dir: str, input_name: str, lang: str):
     write_article.close()
 
 
+def txt_to_paragraph(input_dir: str, input_name: str, lang: str, paragraph_len: int, tokenizer: AutoTokenizer.from_pretrained):
+    """
+    Split the text into
+    :param paragraph_len:
+    :param tokenizer:
+    :param input_dir:
+    :param input_name:
+    :param lang:
+    :return:
+    """
+    nlp = spacy.load(lang)
+    all_stopwords = nlp.Defaults.stop_words
+    words = []
+    bert_para = []
+    makedirs(f"{input_dir}/paragraph", exist_ok=True)
+    write_article = open(f"{input_dir}/paragraph/{input_name}", "w", encoding="UTF-8")
+    counter = 0
+    token_counter = 0
+    with open(f"{input_dir}/{input_name}", "r", encoding="UTF-8") as text_file:
+        for article in tqdm.tqdm(text_file, desc="Formate text in sentece for bert2static"):
+            article = article.replace("\n", "")
+            nlp.max_length = len(article) + 100
+            doc = nlp(article, disable=["ner", "morphologizer"])
+            for token in doc:
+                token_counter += len(tokenizer.tokenize(token.lemma_))
+                if token_counter >= paragraph_len:
+                    write_article.write(f"{' '.join(words)}")
+                    words = [token.lemma_]
+                    token_counter = 0
+                else:
+                    words.append(token.lemma_)
+        write_article.close()
+
+
 if __name__ == "__main__":
-    txt_to_sentence("/home/bagci/data/Wikipedia/Fachbuecher/de/Wirtschaft/wiki_text", "Wirtschaft.txt", "de_core_news_sm")
+    tokenizer = AutoTokenizer.from_pretrained("bert-base-german-cased")
+    txt_to_paragraph("/home/bagci/data/Wikipedia/Fachbuecher/de/Wirtschaft/wiki_text", "Wirtschaft.txt", "de_core_news_sm", 512, tokenizer)
---
+++ src/start_program.py
@@ -0,0 +1,116 @@
+import pickle
+from datetime import datetime
+import Wikipedia_text
+import preprocessing_text_c2static
+import make_vocab_dataset
+import os
+import learn_from_bert_ver2
+import learn_from_bert_ver2_paragraph
+from transformers import AutoTokenizer, BertTokenizer, BertModel
+
+if __name__ == "__main__":
+    switch = {
+        "ef":
+            {
+                "de": "de_core_news_sm",
+                "en": "en_core_web_sm",
+                "fr": "fr_core_news_sm",
+                "da": "da_core_news_sm",
+                "nl": "nl_core_news_sm",
+                "el": "el_core_news_sm",
+                "it": "it_core_news_sm",
+                "zh": "zh_core_web_sm",
+                "ja": "ja_core_news_sm",
+                "lt": "lt_core_news_sm",
+                "nb": "np_core_news_sm",
+                "pl": "pl_core_news_sm",
+                "pt": "pt_core_news_sm",
+                "ro": "ro_core_news_sm",
+                "ru": "ru_core_news_sm",
+                "es": "es_core_news_sm",
+                "multi": "xx_ent_wiki_sm",
+                "default": "xx_ent_wiki_sm",
+            },
+        "ac":
+            {
+                "zh": "zh_core_web_trf",
+                "da": "da_core_news_lg",
+                "nl": "nl_core_news_lg",
+                "en": "en_core_web_trf",
+                "fr": "fr_dep_news_trf",
+                "de": "de_dep_news_trf",
+                "el": "el_core_news_lg",
+                "it": "it_core_news_lg",
+                "ja": "ja_core_news_lg",
+                "lt": "lt_core_news_lg",
+                "nb": "nb_core_news_lg",
+                "pl": "pl_core_news_lg",
+                "pt": "pt_core_news_lg",
+                "ro": "ro_core_news_lg",
+                "ru": "ru_core_news_lg",
+                "es": "es_dep_news_trf",
+                "multi": "xx_sent_ud_sm",
+                "default": "xx_sent_ud_sm",
+            }
+    }
+    bert_models = {
+        "de": "bert-base-german-cased",
+        "en": "bert-base-cased"
+    }
+    for lang in ["de"]:
+        for speciality in ["Wirtschaft"]:
+            # define parameter
+            json_input_dir = f"/home/bagci/data/Wikipedia/Fachbuecher/{lang}/{speciality}/map_to_wiki/{lang}_economy_combined_register.json"
+            wiki_file_input_dir = f"/home/bagci/data/Wikipedia/dewiki"
+            wiki_file_name = f"wikipedia_{lang}.v8.token"
+            dir_output = f"/home/bagci/data/Wikipedia/Fachbuecher/{lang}/{speciality}/wiki_text"
+            spacy_model = switch["ef"][f"{lang}"]
+            tokenizer = AutoTokenizer.from_pretrained("bert-base-german-cased")
+            min_count = 5
+            max_vocab_size = 2000000
+            num_epoch = 5
+            lr = 0.001
+            embeddings_size = 768
+            run_name = datetime.now().strftime('%Y-%m-%d_%H-%M-%S-%f')
+
+            # # Get all Words for language and specialitiy
+            # spec_wiki_id = Wikipedia_text.get_all_article_id(json_input_dir)
+            #
+            # # Get all text for selected articles and write them
+            # Wikipedia_text.save_text_for_ids_and_combine(wiki_file_input_dir, wiki_file_name, dir_output, speciality, spec_wiki_id)
+            #
+            # # Split in sentence
+            # preprocessing_text_c2static.txt_to_sentence(f"{dir_output}", f"{speciality}.txt", spacy_model)
+            #
+            # # Split in paragraphs
+            # preprocessing_text_c2static.txt_to_paragraph(f"{dir_output}", f"{speciality}.txt", spacy_model, 512, tokenizer)
+            #
+            # Convert into Dataset for static embeddings for sen
+            id2word, word2id, id2counts, word_counts = make_vocab_dataset.construct_vocab(f"{dir_output}/sentences/{speciality}.txt", min_count, max_vocab_size)
+
+            if not os.path.isdir(f"{dir_output}/sentences/training_dataset/{run_name}/"):
+                os.makedirs(f"{dir_output}/sentences/training_dataset/{run_name}/", exist_ok=True)
+
+            # creating dataset and vocab
+            pickle.dump(id2word, open(f"{dir_output}/sentences/training_dataset/{run_name}/id2word.p", "wb"))
+            pickle.dump(id2counts, open(f"{dir_output}/sentences/training_dataset/{run_name}/id2counts.p", "wb"))
+            pickle.dump(word_counts, open(f"{dir_output}/sentences/training_dataset/{run_name}/word_counts.p", "wb"))
+            pickle.dump(word2id, open(f"{dir_output}/sentences/training_dataset/{run_name}/word2id.p", "wb"))
+
+            lines, words_locs, num_words = make_vocab_dataset.construct_dataset(f"{dir_output}/sentences/{speciality}.txt", word2id)
+
+            # Saving Dataset
+            with open(f"{dir_output}/sentences/training_dataset/{run_name}/dataset.p", "wb") as f:
+                pickle.dump([lines, words_locs, num_words], f)
+
+            # BERT Model sentences
+            os.system(f"python learn_from_bert_ver2.py --gpu_id 0 --num_epochs {num_epoch} --lr {lr} --algo SparseAdam --t 5e-6 --word_emb_size {embeddings_size} --location_dataset  "
+                      f"{dir_output}/sentences/training_dataset/{run_name}/  --model_folder {dir_output}/sentences/model/{run_name}  "
+                      f"--num_negatives 10 --pretrained_bert_model {bert_models[lang]}")
+
+            os.system(f"python make_vocab_dataset.py --dataset_location {dir_output}/paragraph/{speciality}.txt --min_count {min_count} --max_vocab_size {max_vocab_size} --location_save_vocab_dataset "
+                      f"{dir_output}/paragraph/training_dataset/{run_name}/")
+
+            os.system(f"python learn_from_bert_ver2_paragraph.py --gpu_id 0 --num_epochs {num_epoch} --lr {lr} --algo SparseAdam --t 5e-6 --word_emb_size {embeddings_size} --location_dataset  "
+                      f"{dir_output}/paragraph/training_dataset/{run_name}/  --model_folder {dir_output}/paragraph/model/{run_name}  "
+                      f"--num_negatives 10 --pretrained_bert_model {bert_models[lang]}")
