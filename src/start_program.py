ebd2c88a10e92ccb4e1940038c25bdeecb124a15
Mevluet Bagci <bagci@em.uni-frankfurt.de>
2021-12-14 13:20:03 +0100

    Automatisation

--- src/start_program.py
+++ src/start_program.py
@@ -91,6 +91,8 @@ if __name__ == "__main__":
                 max_vocab_size = 20000000
                 num_epoch = 5
                 lr = 0.001
+                batch_size = 256
+                max_len = 512
                 embeddings_size = 768
                 run_name = datetime.now().strftime('%Y-%m-%d_%H-%M-%S-%f')
 
@@ -125,7 +127,7 @@ if __name__ == "__main__":
                 devive_number = 1
                 # BERT Model sentences
                 os.system(f"python learn_from_bert_ver2.py --gpu_id {devive_number} --num_epochs {num_epoch} --lr {lr} --algo SparseAdam --t 5e-6 --word_emb_size {embeddings_size} --location_dataset  "
-                          f"{os.path.dirname(dir_output)}  --model_folder {os.path.dirname(dir_output)}  "
+                          f"{os.path.dirname(dir_output)}  --model_folder {os.path.dirname(dir_output)}  --batch_size {batch_size} --MAX_LEN {max_len}"
                           f"--num_negatives 10 --pretrained_bert_model {bert_models[lang]}")
 
                 # os.system(f"python make_vocab_dataset.py --dataset_location {dir_output}/paragraph/{speciality}.txt --min_count {min_count} --max_vocab_size {max_vocab_size} --location_save_vocab_dataset "
