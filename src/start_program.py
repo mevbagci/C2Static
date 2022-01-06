178051231c5cc41bc6c3a0989866dc6bb943afd8
Mevluet Bagci <bagci@em.uni-frankfurt.de>
2022-01-06 13:13:00 +0100

    Automatisation

--- src/start_program.py
+++ src/start_program.py
@@ -99,7 +99,7 @@ if __name__ == "__main__":
                 min_count = 10
                 max_vocab_size = 750000
                 num_epoch = 1
-                batch_size = 16
+                batch_size = 256
                 max_len = 512
                 loss_print = 500
                 embeddings_size = 768
